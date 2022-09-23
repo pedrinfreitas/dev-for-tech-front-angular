5cfc681ad85d5f9bf997bae682dddbcb3accbdab
Pedro Freitas <pedrinfreitas@gmail.com>
2022-09-23 19:03:03 -0300

    fix: createUser

--- src/app/demo/components/students/students.module.ts
+++ src/app/demo/components/students/students.module.ts
@@ -1,24 +1,25 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {ButtonModule} from 'primeng/button';
-import {DialogModule} from 'primeng/dialog';
-import {DropdownModule} from 'primeng/dropdown';
-import {FileUploadModule} from 'primeng/fileupload';
-import {InputMaskModule} from 'primeng/inputmask';
-import {InputNumberModule} from 'primeng/inputnumber';
-import {InputTextModule} from 'primeng/inputtext';
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import {RadioButtonModule} from 'primeng/radiobutton';
-import {RatingModule} from 'primeng/rating';
-import {RippleModule} from 'primeng/ripple';
-import {TableModule} from 'primeng/table';
-import {ToastModule} from 'primeng/toast';
-import {ToolbarModule} from 'primeng/toolbar';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
 
-import {StudentsRoutingModule} from './students-routing.module';
-import {StudentsComponent} from './students.component';
-import {CheckboxModule} from "primeng/checkbox";
+import { StudentsRoutingModule } from './students-routing.module';
+import { StudentsComponent } from './students.component';
 
 @NgModule({
     imports: [
@@ -40,6 +41,7 @@ import {CheckboxModule} from "primeng/checkbox";
         StudentsRoutingModule,
         InputMaskModule,
         CheckboxModule,
+        SelectButtonModule,
     ],
     declarations: [StudentsComponent],
 })
